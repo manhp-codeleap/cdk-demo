4b75daf852451122a992975f214ae9355dca034f
Manh Pham <manh.pham@codeleap.de>
2023-01-13 02:31:42 +0700

    add cdk deployment for s3 resource

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+# App (Copy from: https://github.com/mamad-1999/Shoping-card-react?ref=reactjsexample.com)
+
+# Build to S3:
+1. build app
+npm run build
+2. create S3
+3. copy file
+4. create cloud formation distribution & direct to S3 (Remember to update the s3 rule to only allow from CF)
+
+# Next add dynamodb for store the data
--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,16 +1,87 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CanonicalUserPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { ExecSyncOptions, execSync } from 'child_process';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { copySync } from 'fs-extra';
+import { join } from 'path';
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create s3
+    const websiteBucket = new Bucket(this, `${this.stackName}-shop-bucket`, {
+      removalPolicy: RemovalPolicy.RETAIN,
+      bucketName: 'cdk-shop-demo',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create distribution
+    const distribution = new Distribution(
+      this,
+      `${this.stackName}-shop-distribution`,
+      {
+        defaultBehavior: {
+          origin: new S3Origin(websiteBucket),
+        },
+        defaultRootObject: 'index.html',
+      }
+    );
+    // create original access identity
+    const originalAccess = new OriginAccessIdentity(
+      this,
+      `${this.stackName}-cf-identity`,
+      {
+        comment: 'allow access from CF',
+      }
+    );
+    // allow only read access
+    websiteBucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [websiteBucket.arnForObjects('*')],
+        principals: [
+          new CanonicalUserPrincipal(
+            originalAccess.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+    // build react app
+    const execOptions: ExecSyncOptions = {
+      stdio: ['ignore', process.stderr, 'inherit'],
+    };
+
+    const appDir = 'packages/FE/App';
+    const appAsset = Source.asset(join(__dirname, `../${appDir}`), {
+      bundling: {
+        image: cdk.DockerImage.fromRegistry('node:14-alpine'),
+        command: ['sh', '-c', 'npm i && npm run build'],
+        user: 'root',
+        local: {
+          tryBundle(outputDir: string) {
+            execSync(`cd ${appDir} && npm i && npm run build`, execOptions);
+            copySync(join(__dirname, `../${appDir}/build`), `${outputDir}`);
+            return true;
+          },
+        },
+      },
+    });
+
+    // create deployment package
+    new BucketDeployment(this, `${this.stackName}-shop-deployment`, {
+      destinationBucket: websiteBucket,
+      sources: [appAsset],
+      distribution: distribution, // this will invalidate the cache
+    });
+
+    // get the output url
+    new CfnOutput(this, `${this.stackName}-distribution-url`, {
+      value: distribution.distributionDomainName,
+    });
   }
 }
